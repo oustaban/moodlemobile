749965eccf2357586369e373145943b7f3a46486
System Administrator <root@MacBook-Pro.local>
2015-06-22 17:38:12 +0200

    Plugins eleves

---
+++ plugins/eleves/main.js
@@ -0,0 +1,252 @@
+var templates = [
+    "root/externallib/text!root/plugins/eleves/eleves.html",
+    "root/externallib/text!root/plugins/eleves/eleve.html",
+    "root/externallib/text!root/plugins/eleves/eleves_row.html",
+    "root/externallib/text!root/plugins/eleves/countries.json"
+];
+
+define(templates,function (elevesTpl, eleveTpl, elevesRowTpl, countriesJSON) {
+    var plugin = {
+        settings: {
+            name: "eleves",
+            type: "course",
+            menuURL: "#eleves/",
+            lang: {
+                component: "core"
+            },
+            icon: ""
+        },
+
+        storage: {
+            eleve: {type: "model"},
+            eleves: {type: "collection", model: "eleve"}
+        },
+
+        routes: [
+            ["eleves/:courseId", "eleves", "showEleves"],
+            ["eleve/:courseId/:userId", "eleves", "showEleve"],
+            ["eleve/:courseId/:userId/:popup", "eleves_pop", "showEleve"],
+        ],
+
+        limitNumber: 100,
+
+        showEleves: function(courseId) {
+            MM.panels.showLoading('center');
+
+            if (MM.deviceType == "tablet") {
+                MM.panels.showLoading('right');
+            }
+            // Adding loading icon.
+            $('a[href="#eleves/' +courseId+ '"]').addClass('loading-row');
+
+            MM.plugins.eleves._loadEleves(courseId, 0, MM.plugins.eleves.limitNumber,
+                function(users) {
+                    // Removing loading icon.
+                    $('a[href="#eleves/' +courseId+ '"]').removeClass('loading-row');
+
+                    var showMore = true;
+                    if (users.length < MM.plugins.eleves.limitNumber) {
+                        showMore = false;
+                    }
+
+                    MM.plugins.eleves.nextLimitFrom = MM.plugins.eleves.limitNumber;
+
+                    var tpl = {
+                        users: users,
+                        deviceType: MM.deviceType,
+                        courseId: courseId,
+                        showMore: showMore
+                    };
+                    var html = MM.tpl.render(MM.plugins.eleves.templates.eleves.html, tpl);
+
+                    var course = MM.db.get("courses", MM.config.current_site.id + "-" + courseId);
+                    var pageTitle = "";
+
+                    if (course) {
+                        pageTitle = course.get("shortname");;
+                    }
+
+                    MM.panels.show('center', html, {title: pageTitle});
+
+                    // Load the first user
+                    if (MM.deviceType == "tablet" && users.length > 0) {
+                        $("#panel-center li:eq(0)").addClass("selected-row");
+                        MM.plugins.eleves.showEleve(courseId, users.shift().id);
+                        $("#panel-center li:eq(0)").addClass("selected-row");
+                    }
+
+                    // Save the users in the users table.
+                    var newUser;
+                    users.forEach(function(user) {
+                        newUser = {
+                            'id': MM.config.current_site.id + '-' + user.id,
+                            'userid': user.id,
+                            'fullname': user.fullname,
+                            'profileimageurl': user.profileimageurl
+                        };
+                        MM.db.insert('users', newUser);
+                    });
+
+                    // Show more button.
+                    $("#eleves-showmore").on(MM.clickType, function(e) {
+                        var that = $(this);
+                        $(this).addClass("loading-row-black");
+
+                        MM.plugins.eleves._loadEleves(
+                            courseId,
+                            MM.plugins.eleves.nextLimitFrom,
+                            MM.plugins.eleves.limitNumber,
+                            function(users) {
+                                that.removeClass("loading-row-black");
+                                MM.plugins.eleves.nextLimitFrom += MM.plugins.eleves.limitNumber;
+
+                                var tpl = {courseId: courseId, users: users};
+                                var newUsers = MM.tpl.render(MM.plugins.eleves.templates.elevesRow.html, tpl);
+                                $("#eleves-additional").append(newUsers);
+                                if (users.length < MM.plugins.eleves.limitNumber) {
+                                    that.css("display", "none");
+                                }
+                            },
+                            function() {
+                                that.removeClass("loading-row-black");
+                            }
+                        );
+                    });
+
+                }, function(m) {
+                    // Removing loading icon.
+                    $('a[href="#eleves/' +courseId+ '"]').removeClass('loading-row');
+                    if (typeof(m) !== "undefined" && m) {
+                        MM.popErrorMessage(m);
+                    }
+                }
+            );
+        },
+
+        _loadEleves: function(courseId, limitFrom, limitNumber, successCallback, errorCallback) {
+            var data = {
+                "courseid" : courseId,
+                "options[0][name]" : "limitfrom",
+                "options[0][value]": limitFrom,
+                "options[1][name]" : "limitnumber",
+                "options[1][value]": limitNumber,
+            };
+
+            MM.moodleWSCall(
+                'moodle_user_get_users_by_courseid',
+                data,
+                function(users) {
+                    successCallback(users);
+                },
+                {
+                    logging: {
+                        method: 'core_user_view_user_list',
+                        data: {
+                            courseid: courseId
+                        }
+                    }
+                },
+                function(m) {
+                    errorCallback(m);
+                }
+            );
+        },
+
+        showEleve: function(courseId, userId, popUp) {
+            popUp = popUp || false;
+
+            var menuEl = 'a[href="#eleve/' + courseId + '/' + userId + '"]';
+            $(menuEl, '#panel-center').addClass('loading-row-black');
+
+            var data = {
+                "userlist[0][userid]": userId,
+                "userlist[0][courseid]": courseId
+            };
+            MM.moodleWSCall(
+                'moodle_user_get_course_participants_by_id',
+                data,
+                function(users) {
+                    // Load the active user plugins.
+
+                    var userPlugins = [];
+                    for (var el in MM.plugins) {
+                        var plugin = MM.plugins[el];
+                        if (plugin.settings.type == "user") {
+                            if (typeof(plugin.isPluginVisible) == 'function' && !plugin.isPluginVisible()) {
+                                continue;
+                            }
+                            userPlugins.push(plugin.settings);
+                        }
+                    }
+
+                    var newUser = users.shift();
+
+                    var course = MM.db.get("courses", MM.config.current_site.id + "-" + courseId);
+                    var pageTitle = "";
+                    if (course) {
+                        pageTitle = MM.lang.s("eleve");
+                    }
+
+                    var countries = JSON.parse(MM.plugins.eleves.templates.countries.json);
+                    if (newUser.country && typeof countries[newUser.country] != "undefined") {
+                        newUser.country = countries[newUser.country];
+                    }
+
+                    var tpl = {
+                        "user": newUser,
+                        "plugins": userPlugins,
+                        "courseid": courseId,
+                        "popUp": popUp
+                    };
+
+                    var html = MM.tpl.render(MM.plugins.eleves.templates.eleve.html, tpl);
+                    newUser.id = MM.config.current_site.id + "-" + newUser.id;
+                    MM.db.insert("users", newUser);
+
+                    $(menuEl, '#panel-center').removeClass('loading-row-black');
+                    MM.panels.show('right', html, {title: pageTitle});
+                },
+                {
+                    logging: {
+                        method: 'core_user_view_user_profile',
+                        data: {
+                            courseid: courseId,
+                            userid: userId
+                        }
+                    }
+                }
+            );
+        },
+
+        /**
+         * Check if we can show the grades button for this user.
+         * @param  {integer} courseId The course id
+         * @param  {integer} userId   The user Id
+         * @return {boolean}          True or false
+         */
+        _showGrades: function(courseId, userId) {
+            if (MM.plugins.grades.wsName == 'local_mobile_gradereport_user_get_grades_table') {
+                return true;
+            }
+            return false;
+        },
+
+        templates: {
+            "eleve": {
+                model: "eleve",
+                html: eleveTpl
+            },
+            "eleves": {
+                html: elevesTpl
+            },
+            "elevesRow": {
+                html: elevesRowTpl
+            },
+            "countries": {
+                json: countriesJSON
+            }
+        }
+    }
+
+    MM.registerPlugin(plugin);
+});
