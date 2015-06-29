9394d3865ac640c2da0efd3267a9b59ced103c63
System Administrator <root@MacBook-Pro.local>
2015-06-29 15:18:25 +0200

    Debug

--- plugins/eleves/main.js
+++ plugins/eleves/main.js
@@ -26,6 +26,7 @@ define(templates,function (elevesTpl, eleveTpl, elevesRowTpl, countriesJSON) {
             ["eleves/:courseId", "eleves", "showEleves"],
             ["eleve/:courseId/:userId", "eleves", "showEleve"],
             ["eleve/:courseId/:userId/:popup", "eleves_pop", "showEleve"],
+            ["eleve/:courseId/:userId/stop", "eleves", "stopCourse"],
         ],
 
         limitNumber: 100,
@@ -49,9 +50,10 @@ define(templates,function (elevesTpl, eleveTpl, elevesRowTpl, countriesJSON) {
                                 MM.fs.writeInFile(fileEntry, content, 
                                     function(fileUrl) {
                                         MM.log('Write Result :'+fileUrl);
+                                        $('#stopCourse').show();
                                     },
                                     function(fileUrl) {
-                                        MM.log('Write Result NOK:'+json);
+                                        MM.log('Write Result NOK:'+content);
                                     }
                                     
                                 );
@@ -65,6 +67,50 @@ define(templates,function (elevesTpl, eleveTpl, elevesRowTpl, countriesJSON) {
                         //launch offline course
                         MM.plugins.resource._showResource(path);
                     });
+                    
+                    
+        },
+        
+        stopCourse: function(courseId, userId) {
+            var addNote = MM.lang.s("addnote");
+
+            var options = {
+                title: addNote,
+                width: "90%",
+                buttons: {}
+            };
+
+            options.buttons[addNote] = function() {
+
+                var data = {
+                    "notes[0][userid]" : userId,
+                    "notes[0][publishstate]": 'personal',
+                    "notes[0][courseid]": courseId,
+                    "notes[0][text]": $("#addnotetext").val(),
+                    "notes[0][format]": 1
+                }
+
+                MM.widgets.dialogClose();
+                MM.popMessage(MM.lang.s("noteadded"));
+                
+            };
+            options.buttons[MM.lang.s("cancel")] = function() {
+                MM.Router.navigate("participant/" + courseId + "/" + userId);
+                MM.widgets.dialogClose();
+            };
+
+            var rows = 5;
+            var cols = 5;
+            if (MM.deviceType == "tablet") {
+                rows = 15;
+                cols = 50;
+            }
+
+            var html = '\
+            <textarea id="addnotetext" rows="'+rows+'" cols="'+cols+'"></textarea>\
+            ';
+
+            MM.widgets.dialog(html, options);
         },
         
         showEleves: function(courseId) {
