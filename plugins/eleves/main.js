93c3cbf5e2535995b388301df1a52d3bc7580f37
oustaban <oustaban@gmail.com>
2016-09-05 17:50:00 +0200

    Modifs

--- plugins/eleves/main.js
+++ plugins/eleves/main.js
@@ -632,9 +632,9 @@ define(templates,function (elevesTpl, eleveTpl, elevesRowTpl, countriesJSON) {
                               myusers.push(thenewuser);
                               //MM.log('myusers.length:'+myusers.length);
                               var objectWithEvents = $("ul#listeparticipants2 li[eleve='"+$(this).attr('eleve')+"']").detach();
-                              var lihtml = objectWithEvents.html();
-                              lihtml = lihtml.replace('<span id="roweleve">&lt;</span>', '<span id="roweleve">&gt;</span>'); 
-                              objectWithEvents.html(lihtml);
+                              //var lihtml = objectWithEvents.html();
+                              //lihtml = lihtml.replace('<span id="roweleve">&lt;</span>', '<span id="roweleve">&gt;</span>'); 
+                              //objectWithEvents.html(lihtml);
                               
                               if ( $('ul#listeparticipants1').children().length > 0 ) {
                                     var last=0;
@@ -661,10 +661,10 @@ define(templates,function (elevesTpl, eleveTpl, elevesRowTpl, countriesJSON) {
                            });
                            //MM.log('myusers.length:'+myusers.length+'/'+checkbox.val());
                            var objectWithEvents = $("ul#listeparticipants1 li[eleve='"+$(this).attr('eleve')+"']").detach();
-                           var lihtml = objectWithEvents.html();
-                           lihtml = lihtml.replace('<span id="roweleve">&gt;</span>', '<span id="roweleve">&lt;</span>'); 
-                           console.log(lihtml+'/'+objectWithEvents.html());
-                           objectWithEvents.html(lihtml);   
+                           //var lihtml = objectWithEvents.html();
+                           //lihtml = lihtml.replace('<span id="roweleve">&gt;</span>', '<span id="roweleve">&lt;</span>'); 
+                           //console.log(lihtml+'/'+objectWithEvents.html());
+                           //objectWithEvents.html(lihtml);   
                            
                            if ( $('ul#listeparticipants2').children().length > 0 ) {
                                 var last=0;
