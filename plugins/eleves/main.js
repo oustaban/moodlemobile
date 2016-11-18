27ae428dcf6740e1e709c0033f41b1fc95493f39
System Administrator <root@MacBook-Pro-7.local>
2016-11-18 10:22:06 +0100

    Modifs

--- plugins/eleves/main.js
+++ plugins/eleves/main.js
@@ -2815,17 +2815,18 @@ function validerPif(userspif,pifs,course,thisuser,pifsignature1,pifsignature2,pi
         MM.log('pifs length:'+pifs2.length)
         MM.log('pif:'+pifs2[0]+'/'+pifs2[0].scormid);
         if (avant == 1 && (pifsignature1 == 0 || pifsignature2 == 0)) {
-            MM.popErrorMessage("Veuillez signer au bas du tableau, pour valider les compétences à développer dans le cadre du parcours de formation.");
+            MM.popMessage("Veuillez signer au bas du tableau, pour valider les compétences à développer dans le cadre du parcours de formation.");
             valider = 0;
         }
         if (apres == 1 && (pifsignature3 == 0 || pifsignature4 == 0)) {
-            MM.popErrorMessage("Veuillez signer au bas du tableau, pour valider les compétences acquises à l'issue du parcours de formation.");
+            MM.popMessage("Veuillez signer au bas du tableau, pour valider les compétences acquises à l'issue du parcours de formation.");
             valider = 0;
         }
         
     }
     //MM.Router.navigate("eleves/" + course );
     if (valider == 1) {
+        MM.log("Save PIF");
         thisuser.save({pif:pifs2});
         MM.widgets.dialogClose();
     }
