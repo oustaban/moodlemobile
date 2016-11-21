50182f829ebcba4a425794d98c8a84e892e2e738
System Administrator <root@MacBook-Pro-7.local>
2016-11-21 15:23:00 +0100

    Modifs

--- plugins/eleves/main.js
+++ plugins/eleves/main.js
@@ -414,7 +414,7 @@ define(templates,function (elevesTpl, eleveTpl, elevesRowTpl, countriesJSON) {
                         
                         if (pifusercoursewithsignature[0]) {
                             MM.log('Signature Pif pour User:'+user.id+' et cours:'+courseId+' Existe');
-                            var downloadUrl = MM.config.current_site.siteurl + '/local/session/pif/'+courseId+'_'+user.id+'_signature_avant_manager.png';
+                            var downloadUrl = encodeURI(MM.config.current_site.siteurl + '/local/session/pif/'+courseId+'_'+user.id+'_signature_avant_manager.png');
                             var uploadFile = MM.config.current_site.id+"/"+courseId+"/"+user.id+"_pif_manager_avant.png";
                             MM.moodleDownloadFile(downloadUrl, uploadFile,
                                 function(fullpath) {
