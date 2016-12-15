766e167f9ea9a272a0761acef060fe44dd69f673
System Administrator <root@MacBook-Pro-3.local>
2016-12-15 12:02:31 +0100

    Modifs

--- plugins/eleves/main.js
+++ plugins/eleves/main.js
@@ -678,7 +678,7 @@ define(templates,function (elevesTpl, eleveTpl, elevesRowTpl, countriesJSON) {
                                         options2.headers = {
                                           Connection: "close"
                                         };
-                                         MM.fs.fileExists(valuePif,
+                                         MM.fs.fileExists(MM.config.current_site.id+"/"+course+"/"+valuePif,
                                             function(path) {
                                                 var ft = new FileTransfer();
                                                     ft.upload(
