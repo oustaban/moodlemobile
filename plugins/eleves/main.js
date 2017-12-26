f21eee3185e7530f400a9716b0db8fc0cff3b9a7
oustaban <oustaban@gmail.com>
2017-12-26 17:24:36 +0100

    Modifs

--- plugins/eleves/main.js
+++ plugins/eleves/main.js
@@ -1671,6 +1671,7 @@ define(templates,function (elevesTpl, eleveTpl, elevesRowTpl, countriesJSON) {
                         var button = $(this);
                         var course = $(this).attr("course");
                         var user = $(this).attr("user");
+                        var version = $(this).attr("user");
                         var theuser = MM.db.get('users',parseInt(user));
                         MM.log('pif:'+course+'/'+user);
                         
@@ -1713,11 +1714,11 @@ define(templates,function (elevesTpl, eleveTpl, elevesRowTpl, countriesJSON) {
                         var html = '<div id="pifContent"><br/><br/>';
                         html += '<p align="center">Le Protocole Individuel de Formation (PIF) bipartie a bien été initialisée.</p>';
                         html += '<p align="center">Vous pouvez, à présent, former votre stagiaire selon votre rythme.</p><br/><br/>';
-                        html += '<p align="center"><button class="modal-button-1" style="width: 25%">Voir le PIF</button><button class="modal-button-1" style="width: 25%">Modifier le PIF</button></p>';
+                        html += '<p align="center"><button course="'+course+'" user="'+user+'" pif="" version="'+version+'" class="modal-button-1" style="width: 25%">Voir le PIF</button><button id="modifierpif" course="'+course+'" user="'+user+'" pif="" version="'+version+'" class="modal-button-1" style="width: 25%">Modifier le PIF</button></p>';
                         html += '<br/><br/><br/><p align="center">Une fois l\'ensemble du parcours de formation finalisée, vous pourrez compléter la grille<br/> de positionnement ci-dessous en aval de la formation.</p>';
                         html += '<table cellpadding="0" cellspacing="0" width="100%" border="0" class="tablo">';
-                        html += '<tr><td><span class="pifgris">GRILLE DE POSITIONNEMENT</span> <span class="pifnoir">AMONT :</span></td><td> <button class="modal-button-1" style="width: 25%">Voir</button></td></tr>';
-                        html += '<tr><td><span class="pifgris">GRILLE DE POSITIONNEMENT</span> <span class="pifnoir">AVAL :</span></td><td> <button class="modal-button-1" style="width: 25%">Compléter</button></td></tr>';
+                        html += '<tr><td><span class="pifgris">GRILLE DE POSITIONNEMENT</span> <span class="pifnoir">AMONT :</span></td><td> <button class="modal-button-1">Voir</button></td></tr>';
+                        html += '<tr><td><span class="pifgris">GRILLE DE POSITIONNEMENT</span> <span class="pifnoir">AVAL :</span></td><td> <button class="modal-button-1">Compléter</button></td></tr>';
                         html += '</table>';
                         
                         var options = {
@@ -1732,7 +1733,7 @@ define(templates,function (elevesTpl, eleveTpl, elevesRowTpl, countriesJSON) {
                     });
                     
                     //Modifier le Pif button
-                    $('a#modifierpif').on(MM.clickType, function(e) {
+                    $('button#modifierpif').on(MM.clickType, function(e) {
                         MM.log('Modifier pif clicked');
                         var button = $(this);
                         //e.preventDefault();
