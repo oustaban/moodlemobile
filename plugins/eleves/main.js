79e56c1f00221f7f52f10c24a7986d28013c7d8d
oustaban <oustaban@gmail.com>
2017-12-28 13:38:33 +0100

    Modifs

--- plugins/eleves/main.js
+++ plugins/eleves/main.js
@@ -1732,7 +1732,7 @@ define(templates,function (elevesTpl, eleveTpl, elevesRowTpl, countriesJSON) {
                         MM.widgets.dialog(html, options);
                         
                         if (pifArray != "") {
-                            $("button#modifierpif").click();
+                            //$("button#modifierpif").click();
                         }
                         
                     });
