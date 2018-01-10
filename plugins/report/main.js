02f02a114affa81fce9259bac0631bddc7eca910
oustaban <oustaban@gmail.com>
2018-01-10 17:03:02 +0100

    Modifs

--- plugins/report/main.js
+++ plugins/report/main.js
@@ -38,7 +38,9 @@ define(templates,function (reportTpl, elevesRowTpl) {
                     var localCourses = MM.db.where('contents', {'courseid':courseId, 'site':MM.config.current_site.id});
                     
                     for (var key in localCourses) {
-                        MM.log("Porperty:"+key+" Value: "+localCourses[key]);
+                        for(var key2 in localCourses[key]) {
+                            MM.log("Contents: "+key+" Porperty:"+key2+" Value: "+localCourses[key][key2]);
+                        }
                       }
                     var tpl = {
                         users: users,
