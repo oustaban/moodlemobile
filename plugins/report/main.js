7aeae14c3c9cf393c1a3adf6f7265f01bcac423a
oustaban <oustaban@gmail.com>
2018-01-15 17:33:44 +0100

    Modifs

--- plugins/report/main.js
+++ plugins/report/main.js
@@ -46,7 +46,7 @@ define(templates,function (reportTpl, elevesRowTpl) {
                     });
                     
                     MM.log('LOCAL MODULES');
-                    var localModules = MM.db.where('course', {'courseid':courseId, 'site':MM.config.current_site.id});
+                    var localModules = MM.db.where('courses', {'courseid':courseId, 'site':MM.config.current_site.id});
                     $.each(localModules, function( index, value ) {
                             MM.log('LOCAL MODULES');
                             var modules1 = $.grep(value.modules, function( el ) {
