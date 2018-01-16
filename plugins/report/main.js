c3d4fc10fdf0870a23443ca65714c832418a0bbe
oustaban <oustaban@gmail.com>
2018-01-16 12:14:11 +0100

    Modifs

--- plugins/report/main.js
+++ plugins/report/main.js
@@ -65,9 +65,12 @@ define(templates,function (reportTpl, elevesRowTpl) {
                                     MM.log('PROP:'+propertyName+'/'+module1[propertyName]);
                                  }
                                 var modulesValidated = $.grep(module1, function( el ) {
-                                                return el.duration >= localModule.minduration;
+                                                return parseInt(el.duration) >= parseInt(localModule.minduration);
                                 });
                                 MM.log('CHECK');
+                                for(var propertyName in modulesValidated) {
+                                    MM.log('PROPA:'+propertyName+'/'+modulesValidated[propertyName]);
+                                 }
                                 if (modulesValidated[0]) {
                                     modulesUserValidated[user.id].push(modulesValidated[0]);
                                 }
